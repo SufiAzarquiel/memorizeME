4c870c84f1c9085c1b301007f1df3a216ab106e7
Sufi <116170704+SufiAzarquiel@users.noreply.github.com>
2023-10-02 14:12:43 +0200

    Start card comparison feature

--- app/src/main/java/net/azarquiel/memorizeme/MainActivity.kt
+++ app/src/main/java/net/azarquiel/memorizeme/MainActivity.kt
@@ -1,17 +1,29 @@
 package net.azarquiel.memorizeme
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.util.Date
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity(), OnClickListener {
+    private var tagCartaPulsada: Int = 0
+    private var isFirst: Boolean = true
+    private var aciertos: Int = 0
+    private lateinit var ivPrimera: ImageView
     private lateinit var ivPokemon: ImageView
     private lateinit var llVertical: LinearLayout
     private lateinit var random: Random
+    private lateinit var horaInicio: Date
 
     var vPokemon = Array(809) { i -> i + 1 }
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +47,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
     }
 
     private fun newGame() {
+        horaInicio = Date()
         pintarPokemon()
     }
 
@@ -49,8 +62,8 @@ class MainActivity : AppCompatActivity(), OnClickListener {
                 ivPokemon = llHorizontal.getChildAt(j) as ImageView
                 var idFoto =
                     resources.getIdentifier("pokemon${vPokemonEnJuego[x]}", "drawable", packageName)
-                x++
                 ivPokemon.tag = vPokemonEnJuego[x]
+                x++
                 ivPokemon.setBackgroundResource(idFoto)
                 ivPokemon.setImageResource(R.drawable.tapa)
                 ivPokemon.setOnClickListener { pokemOnClick() }
@@ -79,10 +92,88 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         return array
     }
 
-    override fun onClick(p0: View?) {
-        val iv = p0 as ImageView
+    override fun onClick(v: View?) {
+        /* TODO
+         * On first card click, store it
+         * On second card click, compare it to first:
+         *  if equal keep them revealed
+         *  else hide them after a small delay
+         */
+        val iv = v as ImageView
         iv.setImageResource(android.R.color.transparent)
+        tagCartaPulsada = iv.tag as Int
 
+        if (isFirst) {
+            ivPrimera = iv
+            isFirst = false
+        } else {
+            checkCarta(iv)
+        }
+    }
 
+    private fun checkCarta(iview: ImageView) {
+        if (iview.equals(ivPrimera)) {
+            tostada("No pulses la misma manin")
+        } else {
+            if (tagCartaPulsada == ivPrimera.tag as Int) {
+                sumaPunto()
+                iview.isEnabled = false
+                ivPrimera.isEnabled = false
+            } else {
+                GlobalScope.launch() {
+                    SystemClock.sleep(250)
+                    launch(Main) {
+                        ivPrimera.setImageResource(R.drawable.tapa)
+                        iview.setImageResource(R.drawable.tapa)
+                    }
+                }
+            }
+            isFirst = true
+        }
+    }
+
+    private fun sumaPunto() {
+        aciertos++
+        finishGame()
+        if (aciertos == 15) {
+            finishGame()
+        } else {
+            tostada("Has acertado $aciertos veces")
+        }
+    }
+
+    private fun finishGame() {
+        var ahora = Date()
+        var diff = ahora.time - horaInicio.time
+        val seconds = diff / 1000
+        val minutes = seconds / 60
+        val hours = minutes / 60
+        var mensajeFinal = "Felicidades!\nHas tardado: $hours:$minutes:$seconds"
+        AlertDialog.Builder(this)
+            .setTitle("Partida completada")
+            .setMessage(mensajeFinal)
+            .setPositiveButton("Jugar de nuevo") { _, _ ->
+                restartGame()
+            }
+            .setNegativeButton("Salir") { _, _ ->
+                finish()
+            }
+            .show()
+    }
+
+    private fun restartGame() {
+        tagCartaPulsada = 0
+        isFirst = true
+        aciertos = 0
+        for (i in 0 until llVertical.childCount) {
+            var llHorizontal = llVertical.getChildAt(i) as LinearLayout
+
+            llHorizontal.removeAllViews()
+        }
+        newGame()
+    }
+
+    private fun tostada(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 }
